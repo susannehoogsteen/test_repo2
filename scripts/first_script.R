3600ec384b08e2ebd100eacade7354b7e22024ee
Susanne Hoogsteen <s.hoogsteen@student.rug.nl>
2023-11-13 12:21:00 +0100

    fig01 produced plus restore added

--- scripts/first_script.R
+++ scripts/first_script.R
@@ -8,7 +8,23 @@ library(renv)
 # now restore the versions of the packages used to develop this project:
 renv::restore()
 
+# remember to pull in Git tab to get changes from github
+# load libraries
+library(tidyverse)
+library(ggplot2)
+
 x <- c(1,2,3,4,5)
 y <- c(1,4,3,6,9)
 data <- data.frame(x,y)
-data
+fig01 <- data |> ggplot(aes(x=x,y=y))+
+  geom_point() +
+  geom_smooth(method='lm')
+fig01
+# copying the figure is not the best way. It is better to write them into a file with known
+# resolution 
+# ggsave saves the last object into a file.
+# a dot is the root of the porject 
+ggsave("./figures/fig01.png",plot=fig01,width=1920,height=1068,units="px")
+# those dimensions are used because its the resolution of a screen 
+
+# we finished the job. Now save it into github. 
